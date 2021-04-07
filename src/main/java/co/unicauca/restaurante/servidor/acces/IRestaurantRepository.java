65d92aaa5aace19a21cb3adc93297bdccac84634
Luis Arango <lmanuelarango97@outlook.es>
2021-04-07 16:12:01 -0500

    Defini métodos de la Interfaz repositorio de Restaurant

--- src/main/java/co/unicauca/restaurante/servidor/acces/IRestaurantRepository.java
+++ src/main/java/co/unicauca/restaurante/servidor/acces/IRestaurantRepository.java
@@ -5,10 +5,17 @@
  */
 package co.unicauca.restaurante.servidor.acces;
 
+import co.unicauca.restaurante.comunicacion.domain.Restaurant;
+import java.util.List;
+
 /**
- *
+ * Interfaz repositorio de la clase restaurante
  * @author Usuario
  */
 public interface IRestaurantRepository {
-    
+    public Restaurant findRestaurant(String prmresID);
+    public String createRestaurant(Restaurant prmObjRestaurant);
+    public List<Restaurant> findAllRestaurant();
+    public String deleteRestaurant();
+    public String updateRestaurant();
 }
