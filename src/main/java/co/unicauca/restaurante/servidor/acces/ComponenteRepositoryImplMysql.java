3aed3ff848d7e22712e8dc63ce1f896179ffdafd
Luis Arango <lmanuelarango97@outlook.es>
2021-04-07 14:35:01 -0500

    Implemente métodos de conexion

--- src/main/java/co/unicauca/restaurante/servidor/acces/ComponenteRepositoryImplMysql.java
+++ src/main/java/co/unicauca/restaurante/servidor/acces/ComponenteRepositoryImplMysql.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.unicauca.restaurante.servidor.acces;
 
+import co.unicauca.restaurante.comunicacion.domain.Componente;
+import co.unicauca.restaurante.comunicacion.infra.Utilities;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
- * @author Usuario
+ * Clase que implementa la interfaz IComponenteRepository
+ * @author Luis Arango
  */
-public class ComponenteRepositoryImplMysql {
+public class ComponenteRepositoryImplMysql implements IComponenteRepository {
+
+    public Connection conn;
+    
+    //<editor-fold defaultstate="collapsed" desc="Metodos de conexion">
+    /**
+     * Método encargado de realizar la conexión a la base de datos
+     * @return 1 si la conexión fue exitosa, -1 de lo contrario
+     */
+    public int connect() {
+        try {
+            Class.forName(Utilities.loadProperty("server.db.driver"));
+            //crea una instancia de la controlador de la base de datos
+            String url = Utilities.loadProperty("server.db.url");
+            String username = Utilities.loadProperty("server.db.username");
+            String pwd = Utilities.loadProperty("server.db.password");
+            conn = DriverManager.getConnection(url, username, pwd);
+            return 1;
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(ComponenteRepositoryImplMysql.class.getName()).log(Level.SEVERE, "Error al consultar la tabla Dish de la base de datos", ex);
+        }
+        return -1;
+    }
+    
+    /**
+     * Metodo encargado de desconectar la aplicacion de la base de datos.
+     */
+    public void disconnect() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponenteRepositoryImplMysql.class.getName()).log(Level.FINER, "Error al cerrar Connection", ex);
+        }
+    }
+//</editor-fold>
+    
+    @Override
+    public String createComponente(Componente prmObjComponente) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Componente findComponente(int prmcompID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String deleteComponente(int prmcompID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String updateComponente(int prmcompID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
