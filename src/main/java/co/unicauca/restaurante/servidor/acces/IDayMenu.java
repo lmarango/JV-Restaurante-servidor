b2f57cbb003a7de009e26ebfbf42e9c4f92d4571
Luis Arango <lmanuelarango97@outlook.es>
2021-04-07 21:35:49 -0500

    Cree Interfaz IDayMenu

---
+++ src/main/java/co/unicauca/restaurante/servidor/acces/IDayMenu.java
@@ -0,0 +1,15 @@
+
+package co.unicauca.restaurante.servidor.acces;
+
+import co.unicauca.restaurante.comunicacion.domain.DayMenu;
+
+/**
+ *
+ * @author Luis Arangp
+ */
+public interface IDayMenu {
+    public String createDayMenu(DayMenu prmObjMenuDay);
+    public DayMenu findDayMenu(int prmdmenID);
+    public String updateDayMenu(int prmdmenID);
+    public String deleteDayMenu(int prmdemID);
+}
