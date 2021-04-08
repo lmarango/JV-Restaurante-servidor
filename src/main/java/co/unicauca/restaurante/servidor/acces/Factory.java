c588636afc30e8a0d2cc9e5982e1b8870b9b98fc
Luis Arango <lmanuelarango97@outlook.es>
2021-04-07 20:21:40 -0500

    Implemente métodos de factory y documentacion

--- src/main/java/co/unicauca/restaurante/servidor/acces/Factory.java
+++ src/main/java/co/unicauca/restaurante/servidor/acces/Factory.java
@@ -8,14 +8,29 @@ import co.unicauca.restaurante.comunicacion.infra.Utilities;
  * @author Luis Arango
  */
 public class Factory {
+    /**
+     * Instancia de la clase Factory
+     */
     private static Factory instance;
+    /**
+     * Constructor por defecto no parametrizado de la clase Factory
+     */
     private Factory(){}
+    /**
+     * Clase singleton
+     * @return Instancia de la clase factory
+     */
     public static Factory getInstance(){
         if (instance == null) {
             instance = new Factory();
         }
         return instance;
     }
+    /**
+     * Método que crea una instancia concreta de la jerarquia IRestaurantRepository.
+     *
+     * @return una clase hija de la abstracción IRestaurantRepository.
+     */
     public IRestaurantRepository getRepository(){
         String type = Utilities.loadProperty("restaurant.repository");
         if(type.isEmpty()){
@@ -28,6 +43,11 @@ public class Factory {
         }
         return result;
     }
+    /**
+     * Método que crea una instancia concreta de la jerarquia IUserRepository.
+     *
+     * @return una clase hija de la abstracción IUserRepository.
+     */
     public IUserRepository getRepositoryUser(){
         String type = Utilities.loadProperty("restaurant.repository");
         if (type.isEmpty()) {
@@ -39,6 +59,11 @@ public class Factory {
         }
         return result;
     }
+    /**
+     * Método que crea una instancia concreta de la jerarquia IDishRepository.
+     *
+     * @return una clase hija de la abstracción IDishRepository.
+     */
     public IDishRepository getRepositoryDish() {
         String type = Utilities.loadProperty("restaurant.repository");
         if (type.isEmpty()) {
@@ -48,6 +73,24 @@ public class Factory {
         switch (type) {
             case "mysql":
                 result = new DishRepositoryImplMysql();
+                break;
+        }
+        return result;
+    }
+    /**
+     * Método que crea una instancia concreta de la jerarquia IComponenteRepository.
+     *
+     * @return una clase hija de la abstracción IComponenteRepository.
+     */
+    public IComponenteRepository gerRepositoryComponente(){
+        String type = Utilities.loadProperty("restaurant.repository");
+        if (type.isEmpty()) {
+            type = "mysql";
+        }
+        IComponenteRepository result = null;
+        switch (type){
+            case "mysql":
+                result = new ComponenteRepositoryImplMysql();
                 break;
         }
         return result;
