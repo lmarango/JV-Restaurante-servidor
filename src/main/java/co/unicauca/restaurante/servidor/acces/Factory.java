4d151f246cef86c19da5f1845b37da00e3af030e
Luis Arango <lmanuelarango97@outlook.es>
2021-04-07 20:12:36 -0500

    Implemente Factory

--- src/main/java/co/unicauca/restaurante/servidor/acces/Factory.java
+++ src/main/java/co/unicauca/restaurante/servidor/acces/Factory.java
@@ -1,6 +1,8 @@
 
 package co.unicauca.restaurante.servidor.acces;
 
+import co.unicauca.restaurante.comunicacion.infra.Utilities;
+
 /**
  *
  * @author Luis Arango
@@ -13,5 +15,41 @@ public class Factory {
             instance = new Factory();
         }
         return instance;
+    }
+    public IRestaurantRepository getRepository(){
+        String type = Utilities.loadProperty("restaurant.repository");
+        if(type.isEmpty()){
+            type = "mysql";
+        }
+        IRestaurantRepository result = null;
+        
+        switch (type){
+            case "mysql" -> result = new RestaurantRepositoryImplMysql();
+        }
+        return result;
+    }
+    public IUserRepository getRepositoryUser(){
+        String type = Utilities.loadProperty("restaurant.repository");
+        if (type.isEmpty()) {
+            type = "mysql";
+        }
+        IUserRepository result = null;
+        switch (type) {
+            case "mysql" -> result = new UserRepositoryImplMysql();
+        }
+        return result;
+    }
+    public IDishRepository getRepositoryDish() {
+        String type = Utilities.loadProperty("restaurant.repository");
+        if (type.isEmpty()) {
+            type = "mysql";
+        }
+        IDishRepository result = null;
+        switch (type) {
+            case "mysql":
+                result = new DishRepositoryImplMysql();
+                break;
+        }
+        return result;
     }
 }
